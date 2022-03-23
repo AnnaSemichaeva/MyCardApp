1f00da34e89c8885fb709df7ebe8743ad9ec366e
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-23 23:10:43 +0300

    adding an ava

--- MyCardApp/ContentView.swift
+++ MyCardApp/ContentView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(red: 0.09, green: 0.63, blue: 0.52)
-            Text("Anna Semichaeva")
+            Color(red: 0.09, green: 0.63, blue: 0.52).ignoresSafeArea()
+            VStack {
+                Image("ava").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/).frame(width: 150, height: 150).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                Text("Anna Semichaeva")
+            }
         }
         
     }
