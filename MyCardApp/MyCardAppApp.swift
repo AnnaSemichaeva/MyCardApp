e4906a5e9583c420d9135f25d384d016bbad1d7b
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-23 21:01:00 +0300

    Initial commit

---
+++ MyCardApp/MyCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardAppApp.swift
+//  MyCardApp
+//
+//  Created by macuser on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
